950794c76bf9cb46b8cd525b0e002112b166ed6a
Klaussia <baussas@yandex.ru>
2023-02-08 22:48:58 +0300

    ex2 Create Method

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -1,8 +1,12 @@
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -11,7 +15,8 @@ import java.net.URL;
 public class Settings {
 
     protected AndroidDriver driver;
-    String URL = "http://127.0.0.1:4723/wd/hub";
+    String AppiumServer = "127.0.0.1";
+    String AppiumPort = "4723";
     String deviceName = "emulator-5554";
     String androidVersion = "8.1";
 
@@ -21,23 +26,45 @@ public class Settings {
         capabilities.setCapability("deviceName", deviceName);
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", androidVersion);
-        capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
 
-        File file = new File("src/main/resources/", "wikipedia.apk");
+        File file = new File("src/main/resources", "org.wikipedia.apk");
         capabilities.setCapability("app", file.getAbsolutePath());
 
-        driver = new AndroidDriver(new URL(URL), capabilities);
+        driver = new AndroidDriver(new URL("http://" + AppiumServer + ":" + AppiumPort + "/wd/hub"), capabilities);
     }
 
     @Test
     public void testStart(){
-        System.out.println("Test completed");
+        clickElement(By.id("org.wikipedia:id/search_container"));
+//        fillText(By.id("org.wikipedia:id/search_src_text"), "Hello");    /** Для проверки */
+        checkElement(By.id("org.wikipedia:id/search_src_text"));
     }
 
-//    @AfterEach
-//    public void testFinish() {
-//        driver.quit();
-//    }
+    public void setWait(By elementBy){
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elementBy));
+    }
+    public void clickElement(By elementBy){
+        setWait(elementBy);
+        driver.findElement(elementBy).click();
+    }
+    public void checkElement(By elementBy){
+        setWait(elementBy);
+        WebElement element = driver.findElement(elementBy);
+        String getWord = element.getText();
+        String checkWord = "Search…";
+        String yourWord = "Line contain: " + getWord + " word";
+        String error_message = "This line does not contain the required text: Your word is " + getWord;
+        System.out.println(yourWord);
+        Assertions.assertEquals(checkWord, getWord, error_message);
+    }
+
+    public void fillText(By elementBy, String text){
+        setWait(elementBy);
+//        Assertions.assertTrue(driver.findElement(elementBy).isDisplayed());
+        driver.findElement(elementBy).sendKeys(text);
+    }
+
 }
