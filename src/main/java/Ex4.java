438cb3cfc2de6b69c8bf4d80635acaae676382f3
Klaussia <baussas@yandex.ru>
2023-02-13 21:47:41 +0300

    Ex4

--- src/main/java/Ex4.java
+++ src/main/java/Ex4.java
@@ -12,11 +12,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 
-public class Settings {
-
+public class Ex4 {
     protected AndroidDriver driver;
     String AppiumServer = "127.0.0.1";
     String AppiumPort = "4723";
@@ -32,7 +32,7 @@ public class Settings {
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
 
-        File file = new File("src/main/resources", "org.wikipedia.apk");
+        File file = new File("src/main/resources/", "org.wikipedia.apk");
         capabilities.setCapability("app", file.getAbsolutePath());
 
         driver = new AndroidDriver(new URL("http://" + AppiumServer + ":" + AppiumPort + "/wd/hub"), capabilities);
@@ -41,9 +41,23 @@ public class Settings {
     @Test
     public void testStart(){
         clickElement(By.id("org.wikipedia:id/search_container"));
-//        fillText(By.id("org.wikipedia:id/search_src_text"), "JVM");    /** Для проверки */
-        assertElementHasText(By.id("org.wikipedia:id/search_src_text"), "Search…", "This line does not contain the required text");
+        fillText(By.id("org.wikipedia:id/search_src_text"), "JAVA");
+        hideKeyboard();
+        assertResultHaveJava(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"));
     }
+
+
+    public void assertResultHaveJava(By elementBy){
+        setWait(elementBy);
+        List<MobileElement> test = driver.findElements(elementBy);
+        String checkWord = "Java";
+        String errorMessage = "Error, doesn't found word: " + checkWord + " in every response";
+        for(MobileElement mobileElement : test){
+            String text = mobileElement.getText();
+            Assertions.assertTrue(text.startsWith(checkWord), errorMessage);
+        }
+    }
+
 
     public void setWait(By elementBy){
         WebDriverWait wait = new WebDriverWait(driver, 20);
@@ -53,18 +67,12 @@ public class Settings {
         setWait(elementBy);
         driver.findElement(elementBy).click();
     }
-    public void assertElementHasText(By elementBy, String checkWord, String error_message){
-        setWait(elementBy);
-        WebElement element = driver.findElement(elementBy);
-        String getWord = element.getText();
-        String yourWord = "Line contain: " + getWord + " word";
-        System.out.println(yourWord);
-        Assertions.assertEquals(checkWord, getWord, error_message);
+    public void hideKeyboard(){
+        driver.hideKeyboard();
     }
 
     public void fillText(By elementBy, String text){
         setWait(elementBy);
         driver.findElement(elementBy).sendKeys(text);
     }
-
 }
